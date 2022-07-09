5e79909221cec08eeb991aca10534beb8a45f22b
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-07-09 16:04:36 +0300

    Add solutions for exercise 2.2

--- part2/courseinfo/src/component/Course.js
+++ part2/courseinfo/src/component/Course.js
@@ -1,9 +1,11 @@
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({course}) => {
     return (
     <div>
     <Content parts = {course.parts}/>
+    <Total parts = {course.parts}/>
     </div>
     )
 }
---
+++ part2/courseinfo/src/component/Total.js
@@ -0,0 +1,12 @@
+const Total = ({parts}) => {
+
+    const total = parts.reduce((sum, part) => {
+        return sum + part.exercises
+    }, 0)
+
+    return (
+        <p><b>total of {total} exercises</b></p>
+    )
+}
+
+export default Total
