8b0cef4846e9adb46cfb519ab80bf90af9fc57a4
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-07-15 10:09:25 +0300

    Fix the error that creates a problem in filtering

--- part2/phonebook/src/components/GetPersons.js
+++ part2/phonebook/src/components/GetPersons.js
@@ -1,11 +1,14 @@
 const RenderPersons = ({persons, searchBy}) => {
 
+  const filteredPersons = persons.filter(person => {
+    return (person.name.toLowerCase().includes(searchBy.toLowerCase()))})
+
   if (searchBy === "") {
     return (persons.map(person => <p key={person.name}>{person.name} {person.number}</p>));
-  } else {
-    const filteredPersons = persons.filter(person => 
-        {(person.name.toLowerCase().includes(searchBy.toLowerCase()))});
 
+  } else {
     return (filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>))
   } 
 }
+
+export default RenderPersons
