bae5c2754f7d22db660db5732e9e831e4a2f374f
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-07-29 10:39:32 +0300

    Add solution for Phonebook backend step 7

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const { application } = require('express')
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
+
 
 app.use(express.json())
 
