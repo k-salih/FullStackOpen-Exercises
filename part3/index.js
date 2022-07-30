12b95ce08e41bb0faef8c7ebfdd0895f91d0c586
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-07-30 23:14:23 +0300

    Add production build for frontend

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
