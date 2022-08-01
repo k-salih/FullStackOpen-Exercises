922d28c82203d644d7d5eb9aab88864586f4b9f0
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-08-01 16:59:46 +0300

    Add solution for the Phonebook Database Step 3

--- part3/index.js
+++ part3/index.js
@@ -63,10 +63,11 @@ app.get('/api/persons/:id', (request, response) => {
     })
 
 app.delete('/api/persons/:id', (request,response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
+})
 
 const generateId = () => {
     const id = Math.random() * 1000000
