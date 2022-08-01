d63dd567e548ea264f4f10f1a80779c8fbca23cb
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-08-01 15:36:39 +0300

    Add solution for the Phonebook Database Step 2

--- part3/index.js
+++ part3/index.js
@@ -83,14 +83,14 @@ app.post('/api/persons', (request, response) => {
       return response.status(400).json({ error: 'Name must be unique' })
 
     } else {
-      const person = {
-        id: generateId(),
+      const person = new Person({
         name: body.name,
         number: body.number
-      }
-      persons = persons.concat(person)
-      response.json(person)
-    }
+      })
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
+    } 
   })
 
 const PORT = process.env.PORT
