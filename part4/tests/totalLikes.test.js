36cad37d091b2e10d2b91325d0f65bbab02146d2
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-08-03 14:58:27 +0300

    Add solutions for exercise 4.4

---
+++ part4/tests/totalLikes.test.js
@@ -0,0 +1,17 @@
+const totalLikes = require('../utils/list_helper').totalLikes
+
+describe('totalLikes', () => {
+  test('of one value is the value itself', () => {
+    const blogs = [{ likes: 1 }]
+    expect(totalLikes(blogs)).toBe(1)
+  })
+
+  test('of many is calculated right', () => {
+    const blogs = [{ likes: 1 }, { likes: 2 }, { likes: 3 }]
+    expect(totalLikes(blogs)).toBe(6)
+  })
+
+  test('of empty array is zero', () => {
+    expect(totalLikes([])).toBe(0)
+  })
+})
