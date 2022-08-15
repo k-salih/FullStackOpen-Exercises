0dceff8ec455b6521da4f12a0adc4cbc78f32f85
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-08-15 21:58:29 +0300

    Add solution for the bloglist expansion step 5 (4.17)

--- part4/models/user.js
+++ part4/models/user.js
@@ -4,6 +4,12 @@ const userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true, minLength: 3},
   name: String,
   passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
 })
 
 userSchema.set('toJSON', {
