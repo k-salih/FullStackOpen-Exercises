c906ebb4713576279b2cb44350fe9b072c1440b0
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-08-17 09:35:42 +0300

    Add solution for the bloglist expansion step 9 (4.21)

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -33,6 +33,18 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  } 
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'user not authorized' })
+  }
+
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
