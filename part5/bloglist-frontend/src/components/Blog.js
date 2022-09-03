cbae3a3d6a7f74842836147269c3e90a8a560bfd
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-09-03 12:37:50 +0300

    Add solution for the bloglist frontend step 7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisibility = () => {
+      setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  return (
+      <div style={blogStyle}>
+          <div>
+              {blog.title} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+          </div>
+          <div style={{ display: visible ? '' : 'none' }}>
+              {blog.url} <br />
+              likes {blog.likes} <button>like</button> <br />
+              {blog.author}
+          </div>
+      </div>
+  )
+}
 
 export default Blog
