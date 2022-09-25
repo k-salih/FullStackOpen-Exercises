192a228e1e08086b55229ccb32f1105225cebedb
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-09-25 23:21:57 +0300

    Add solution for the anecdotes step 10 (6.12)

--- part6/redux-anecdotes/src/components/AnectodeList.js
+++ part6/redux-anecdotes/src/components/AnectodeList.js
@@ -1,12 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 import { setNotification, clearNotification } from '../reducers/notificationReducer'
-
-
+import Filter from './Filter'
 
 const AnectodeList = () => {
-    const unorderedAnecdotes = useSelector(state => state.anecdotes)
+    const unorderedAnecdotes = useSelector(state => {
+        if (state.filter === '') {
+            return state.anecdotes
+        }
+        return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    })
+    
     const anecdotes = unorderedAnecdotes.slice().sort((a, b) => b.votes - a.votes)
+    
     const dispatch = useDispatch()
 
     const voteAnecdote = (anecdote) => {
@@ -20,6 +26,7 @@ const AnectodeList = () => {
     return (
         <div>
             <h2>Anecdotes</h2>
+            <Filter />
             {anecdotes.map(anecdote =>
                 <div key={anecdote.id}>
                 <div>
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,11 +1,13 @@
 import reducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import { configureStore} from '@reduxjs/toolkit'
+import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
     reducer: {
         anecdotes: reducer,
-        notification: notificationReducer
+        notification: notificationReducer,
+        filter: filterReducer
     }
 })
 
