efa40a9c89589df61eaaca740cb3e22f82fc57bd
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2022-11-28 22:45:34 +0300

    Update gitignore for backend

---
+++ part7/bloglist-expansion/backend/controllers/blogs.js
@@ -0,0 +1,66 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog.toJSON())
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+blogsRouter.post('/', userExtractor, async (request, response) => {
+  const body = request.body
+  const user = request.user
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
+    response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'user not authorized' })
+  }
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+
+  const blog = {
+    title: request.body.title,
+    likes: request.body.likes,
+    author: request.body.author,
+    url: request.body.url,
+    user: user._id
+  }
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
